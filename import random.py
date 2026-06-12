e8d420e95cafbfc9dbdb4adeb9c21498f1cbea32
KATGOWSRI123 <gowtham11a2akr@gmail.com>
2026-06-12 19:50:28 +0530

    Update import random.py

--- import random.py
+++ import random.py
@@ -1,21 +1,40 @@
 import random 
-x = input("odd  or even   :")
-x1= int(input("enter a number  :"))
+while True:
+    x = input("odd or even: ").lower()
+    if x in ["odd", "even"]:
+        break
+    print("Enter only odd or even")
+while True:
+    try:
+        x1 = int(input("enter a number (0-10): "))
+        if 0 <= x1 <= 10:
+            break
+        print("Enter a number between 0 and 10")
+    except ValueError:
+        print("Enter a valid number")
 
 y=random.randint(0,10)
 print("bot enter number is " ,y)
 z=x1+y
-if z %2 ==0 and x=="even":
+if (z %2 ==0 and x=="even") or (z%2 ==1 and x=="odd"):
     print ("the number is ",z ,"u won the toss")
-    a=input("batting or bowling  :")
+    while True:
+        a = input("batting or bowling: ").lower()
+        if a in ["batting", "bowling"]:
+            break
+        print("Enter batting or bowling only")
     if a=="batting":
         print("u chose batting and bot is bowling")
         i=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
+            if 0<=x<=10:
                 if x==y:
                     print ("out")
                     print("total score is ",i)
@@ -28,37 +47,47 @@ if z %2 ==0 and x=="even":
         print("now bot is battting")
         q=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",q)
-                    if q>i:
-                        a1=q-i
-                        print ("bot won by ",a1)
-                        break
-                    else:
-                        a1=i-q
-                        print("u won by ",a1)
+            if 0<=x<=10:
+                if x == y:
+                    print("OUT")
+                    print("Total score is", q)
 
-                        break
-                if q>i:
-                    a1=q-i
-                    print ("bot won by ",a1)
+                    if q > i:
+                        print("Bot won by", q - i)
+                    elif q < i:
+                        print("You won by", i - q)
+                    else:
+                        print("Match tied")
+
+                    break
                 else:
-                    q+=y
+                    q += y
+
+                if q > i:
+                    print("Bot won by", q - i)
+                    break
+
             else:
                 print("out of range")
-    if a== "bowling":
+    elif a== "bowling":
         print("now bot is batting")
         i=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
+            if 0<=x<=10:
                 if x==y:
                     print ("out")
                     print("total score is ",i)
@@ -71,123 +100,35 @@ if z %2 ==0 and x=="even":
         print("now bot is bowling")
         q=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",q)
-                    if q>i:
-                        a1=q-i
-                        print ("you won by ",a1)
-                        break
+            if 0<=x<=10:
+                if x == y:
+                    print("OUT")
+                    print("Total score is", q)
+
+                    if q > i:
+                        print("You won by", q - i)
+                    elif q < i:
+                        print("Bot won by", i - q)
                     else:
-                        a1=i-q
-                        print("bot won by ",a1)
-
-                        break
-                if q>i:
-                    a1=q-i
-                    print ("you won by ",a1)
-                else:
-                    q+=x
-            else:
-                print("out of range")
-
-
-            
-elif z%2 ==1 and x=="odd":
-    print ("the number is ",z," u won the toss")
-    a= input("batting or bowling   :")
-    if a=="batting":
-        print("u chose batting and bot is bowling")
-        i=0
-        while True:
-            x=int(input("enter a number (0 to 10)   :"))
-            y=random.randint(0,10)
-            print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",i)
+                        print("Match tied")
 
                     break
                 else:
-                    i+=x
-            else:
-                print("out of range")
-        print("now bot is battting")
-        q=0
-        while True:
-            x=int(input("enter a number (0 to 10)   :"))
-            y=random.randint(0,10)
-            print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",q)
-                    if q>i:
-                        a1=q-i
-                        print ("bot won by ",a1)
-                        break
-                    else:
-                        a1=i-q
-                        print("u won by ",a1)
+                    q += x
 
-                        break
-                if q>i:
-                    a1=q-i
-                    print ("bot won by ",a1)
-                else:
-                    q+=y
-            else:
-                print("out of range")  
-    if a== "bowling":
-        print("now bot is batting")
-        i=0
-        while True:
-            x=int(input("enter a number (0 to 10)   :"))
-            y=random.randint(0,10)
-            print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",i)
-
+                if q > i:
+                    print("You won by", q - i)
                     break
-                else:
-                    i+=y
             else:
                 print("out of range")
-        print("now bot is bowling")
-        q=0
-        while True:
-            x=int(input("enter a number (0 to 10)   :"))
-            y=random.randint(0,10)
-            print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",q)
-                    if q>i:
-                        a1=q-i
-                        print ("you won by ",a1)
-                        break
-                    else:
-                        a1=i-q
-                        print("bot won by ",a1)
-
-                        break
-                if q>i:
-                    a1=q-i
-                    print ("you won by ",a1)
-                else:
-                    q+=x
-            else:
-                print("out of range")              
             
-
 else :
     print("the number is ",z,"u loss the toss")
     a=random.choice(["batting","bowling"])
@@ -196,10 +137,14 @@ else :
         print("now bot is batting")
         i=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
+            if 0<=x<=10:
                 if x==y:
                     print ("out")
                     print("total score is ",i)
@@ -212,37 +157,46 @@ else :
         print("now bot is bowling")
         q=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",q)
-                    if q>i:
-                        a1=q-i
-                        print ("you won by ",a1)
-                        break
-                    else:
-                        a1=i-q
-                        print("bot won by ",a1)
+            if 0<=x<=10:
+                if x == y:
+                    print("OUT")
+                    print("Total score is", q)
 
-                        break
-                if q>i:
-                    a1=q-i
-                    print ("you won by ",a1)
+                    if q > i:
+                        print("You won by", q - i)
+                    elif q < i:
+                        print("Bot won by", i - q)
+                    else:
+                        print("Match tied")
+
+                    break
                 else:
-                    q+=x
+                    q += x
+
+                if q > i:
+                    print("You won by", q - i)
+                    break
             else:
                 print("out of range")
-    if a== "bowling":
+    elif a== "bowling":
         print("u chose bowling and bot is batting")
         i=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
+            if 0<=x<=10:
                 if x==y:
                     print ("out")
                     print("total score is ",i)
@@ -255,26 +209,31 @@ else :
         print("now bot is bowling")
         q=0
         while True:
-            x=int(input("enter a number (0 to 10)   :"))
+            try:
+                x = int(input("enter a number (0 to 10): "))
+            except ValueError:
+                print("Enter a valid number")
+                continue
             y=random.randint(0,10)
             print(y)
-            if x<=10:
-                if x==y:
-                    print ("out")
-                    print("total score is ",q)
-                    if q>i:
-                        a1=q-i
-                        print ("you won by ",a1)
-                        break
-                    else:
-                        a1=i-q
-                        print("bot won by ",a1)
+            if 0<=x<=10:
+                if x == y:
+                    print("OUT")
+                    print("Total score is", q)
 
-                        break
-                if q>i:
-                    a1=q-i
-                    print ("you won by ",a1)
+                    if q > i:
+                        print("Bot won by", q - i)
+                    elif q < i:
+                        print("You won by", i - q)
+                    else:
+                        print("Match tied")
+
+                    break
                 else:
-                    q+=x
+                    q += x
+
+                if q > i:
+                    print("Bot won by", q - i)
+                    break
             else:
                 print("out of range")
